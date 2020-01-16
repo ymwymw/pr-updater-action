555903128747c77cb065c64b6b65a5ec14b006a4
ymwymw <31081824+ymwymw@users.noreply.github.com>
2020-01-16 00:41:24 -0500

    added logging

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,19 @@ const token = core.getInput('token')
 const client = new github.GitHub(token)
 
 async function main() {
+    const pullsResponse = await client.pulls.list({
     const baseBranch = github.context.payload.ref
+    console.log(`Merging ${baseBranch} into branches with open pull requests`)
     const pullsResponse = await client.pulls.list({
         ...github.context.repo,
         base: baseBranch,
         state: 'open',
     })
     const prs = pullsResponse.data
+    console.log(`Branches: ${prs.length}`)
+    prs.forEach(function (pr) {
+        console.log(pr)
+    })
     await Promise.all(
         prs.map((pr) => {
             client.pulls.updateBranch({
